d8a0059436b6e46c024e1f2fa13160a9ffbe1e0b
Justin Szeluga <justin.szeluga@gmail.com>
2018-12-26 19:07:44 -0500

    Add first pass at CellDimension

--- src/main/java/com/github/jszeluga/entity/dimension/CellDimension.java
+++ src/main/java/com/github/jszeluga/entity/dimension/CellDimension.java
@@ -1,9 +1,24 @@
 package com.github.jszeluga.entity.dimension;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "CELL_DIM")
 public class CellDimension {
+
+  @Id
+  @Column(name = "cell_key")
+  private long cellKey;
+
+
+  public long getCellKey() {
+    return cellKey;
+  }
+
+  public void setCellKey(final long cellKey) {
+    this.cellKey = cellKey;
+  }
 }
