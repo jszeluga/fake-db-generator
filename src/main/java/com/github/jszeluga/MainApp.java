f6618a1379726eb43a9d6e535626b331d42b37c5
Justin Szeluga <justin.szeluga@gmail.com>
2018-12-27 13:20:28 -0500

    Add method to generate a stream of records that will be inserted into the database

--- src/main/java/com/github/jszeluga/MainApp.java
+++ src/main/java/com/github/jszeluga/MainApp.java
@@ -2,11 +2,28 @@ package com.github.jszeluga;
 
 import com.github.jszeluga.util.HibernateTransaction;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class MainApp {
 
   public static void main(String[] args){
 
     //do stuff
     HibernateTransaction.closeSessionFactory();
+  }
+
+  private static <T> Stream<T> generateStreamForClass(Class<T> clazz, int total){
+    List<T> recs = new ArrayList<>();
+    for(int i=0; i<total; i++){
+      try {
+        recs.add(clazz.newInstance());
+      } catch (Exception e){
+        throw new RuntimeException(e);
+      }
+    }
+
+    return recs.stream();
   }
 }
