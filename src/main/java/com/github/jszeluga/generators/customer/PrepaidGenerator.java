b34d44f1a5953372e753a3a0c6a2dbd5c1f1ec1b
Justin Szeluga <justin.szeluga@gmail.com>
2018-12-27 17:44:00 -0500

    Add Prepaid generator

---
+++ src/main/java/com/github/jszeluga/generators/customer/PrepaidGenerator.java
@@ -0,0 +1,17 @@
+package com.github.jszeluga.generators.customer;
+
+import com.github.jszeluga.entity.dimension.CustomerDimension;
+import com.github.jszeluga.generators.AbstractGenerator;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class PrepaidGenerator extends AbstractGenerator<CustomerDimension> {
+
+    @Override
+    public void accept(CustomerDimension customerDimension) {
+        if(customerDimension != null){
+            boolean isPrepaid = ThreadLocalRandom.current().nextBoolean();
+            customerDimension.setPrePaid(isPrepaid);
+        }
+    }
+}
